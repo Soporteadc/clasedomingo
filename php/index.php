2abfd6ecb2c79177c709cac7828adeefbe892cff
arturodavid <aadrian02@hotmail.com>
2016-01-10 18:24:31 -0500

    first commit

---
+++ php/index.php
@@ -0,0 +1,21 @@
+<?php 
+$numero1=2;
+$numero2=64;
+$suma = $numero1+$numero2;
+$resta = $numero1-$numero2;
+$multiplicar = $numero1*$numero2;
+$division = $numero1/$numero2;
+echo "la suma es: ";
+echo $suma;
+echo "<br>";
+echo "la resta es: ";
+echo $resta;
+echo "<br>";
+echo "la multiplicacion es: ";
+echo $multiplicar;
+echo "<br>";
+echo "la division es: ";
+echo $division;
+
+
+ ?>
---
+++ php/miperfil.php
@@ -0,0 +1,11 @@
+<?php 
+echo "Nombre: Edgar";
+echo "<br>";
+echo "Apellidos: Cruz Aranda";
+echo "<br>";
+echo "Fecha de Nacimiento: 09-04-1976";
+echo "<br>";
+$anac=1976;
+echo "Mi edad es: " . (date('Y') -$anac);
+
+ ?>
